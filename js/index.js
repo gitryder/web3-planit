104f82126552a847c22e3f64cfa672aa4a616882
Danyl Fernandes <realllydan@gmail.com>
2022-09-21 03:27:14 +0530

    :sparkles: Use newer web3 APIs

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const todoForm = document.getElementById("todo-form");
+const todoList = document.getElementById("todo-list");
 
 if (typeof web3 !== "undefined") {
   web3 = new Web3(web3.currentProvider);
@@ -6,86 +7,82 @@ if (typeof web3 !== "undefined") {
   web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
 }
 
-web3.eth.defaultAccount = web3.eth.accounts[0];
-
-// Set Contract Abi
-const contractAbi = [
-  {
-    inputs: [
-      {
-        internalType: "string",
-        name: "todo",
-        type: "string",
-      },
-    ],
-    name: "addTodo",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "getAllTodos",
-    outputs: [
-      {
-        internalType: "string[]",
-        name: "",
-        type: "string[]",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    name: "todos",
-    outputs: [
-      {
-        internalType: "string",
-        name: "",
-        type: "string",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-];
-
-const contractAddress = "0xBC09b448346E64cFda7Fb9FfE055667E1ce0B33f";
-
-const contract = web3.eth.contract(contractAbi).at(contractAddress);
-
 /*
+
+1. Preparing the required credentials & creating the contract instance
+===
+
+web3.eth.defaultAccount = "";
+
+// The ABI of the contract, after compiling it.
+const contractAbi = [];
+
+// The address where the contract is deployed.
+const contractAddress = "";
+
+const contract = new web3.eth.Contract(contractAbi, contractAddress);
+*/
+
+/**
+
+ 2. Populating the todos
+ ===
+
 function populateTodos() {
-  const todos = contract.getAllTodos.call((err, todos) => {
-    todos.forEach((todo) => {
-      if (todo !== "") {
-        const todoList = document.getElementById("todo-list");
+  contract.methods
+    .getAllTodos()
+    .call()
+    .then((todos) => {
+      console.log(todos);
+      todos.forEach((todo) => {
         const todoElement = document.createElement("li");
         todoElement.className = "list-item";
         todoElement.innerText = todo;
         todoList.appendChild(todoElement);
-      }
+      });
     });
-  });
 }
 */
 
-// function handleTodoFormSubmit(event) {
-//   event.preventDefault();
+/**
 
-//   const todoFormData = new FormData(todoForm);
-//   const todoContent = todoFormData.get("todo");
+3. Clearing the todos
+===
 
-//   contract.addTodo(todoContent);
-//   window.location.reload();
-// }
+function clearTodos() {
+  todoList.innerHTML = "";
+}
+*/
+
+/**
+ 
+ 4. Getting the submitted todo and adding it to the blockchain
+ ===
+
+function handleTodoFormSubmit(event) {
+  event.preventDefault();
+
+  const todoFormData = new FormData(todoForm);
+  const todoContent = todoFormData.get("todo");
+
+  console.log("account", web3.eth.defaultAccount);
+
+  contract.methods
+    .addTodo(todoContent)
+    .send({ from: web3.eth.defaultAccount })
+    .then(() => {
+      clearTodos();
+      populateTodos();
+    });
+}
+*/
 
 // populateTodos();
-// todoForm.addEventListener("submit", handleTodoFormSubmit);
+
+/**
+
+5. Listening to the submit event emitted by the form
+===
+
+todoForm.addEventListener("submit", handleTodoFormSubmit);
+*/
